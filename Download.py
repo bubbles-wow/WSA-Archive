402349510fb681c4750a19da91671a8fb5f0df2e
bubbles-wow <116588240+bubbles-wow@users.noreply.github.com>
2023-08-08 16:19:13 +0800

    Add files via upload

--- Download.py
+++ Download.py
@@ -25,8 +25,7 @@ class Prop(OrderedDict):
         return '\n'.join(f'{item}={self[item]}' for item in self)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-#dir = Path.cwd().parent
-dir = "/home/runner/work/WSA-Archive/WSA-Archive/./WSA-Archive"
+dir = Path.cwd().parent
 user_token = ""
 ReleaseType = "retail"
 UpdateID = "00000000-0000-0000-0000-000000000000"
@@ -46,9 +45,8 @@ try:
 except:
     print("Notice: You haven't logged in yet. Some UpdateID may not be available.")
     time.sleep(1)
-if Path.exists(dir + "/UpdateID.cfg"):
-    print("Found UpdateID.cfg!")
-    with open(dir + "/UpdateID.cfg", "r") as f:
+if Path.exists(dir / "WSA-Archive/./WSA-Archive/UpdateID.cfg"):
+    with open(dir / "WSA-Archive/./WSA-Archive/UpdateID.cfg", "r") as f:
         text = f.read()
         Version = Prop(text).get("Version")
         UpdateID = Prop(text).get("UpdateID")
@@ -65,7 +63,7 @@ else:
         exit()
 Filename = "MicrosoftCorporationII.WindowsSubsystemForAndroid_" + Version + "_neutral_~_8wekyb3d8bbwe.Msixbundle"
 print(Filename)
-with open(dir + "/FE3FileUrl.xml", "r") as f:
+with open(dir / "WSA-Archive/./WSA-Archive/FE3FileUrl.xml", "r") as f:
     FE3_file_content = f.read()
     f.close()
 try:
@@ -88,10 +86,10 @@ for l in doc.getElementsByTagName("FileLocation"):
     if url.split("/")[2] == "tlu.dl.delivery.mp.microsoft.com":
         print(url)
         break
-with open(dir + "/download/" + Filename, "wb") as f, requests.get(url, stream=True) as res:
+with open(dir / "download/" + Filename, "wb") as f, requests.get(url, stream=True) as res:
     for chunk in res.iter_content(chunk_size=4*1024):
         if chunk:
             f.write(chunk)
             f.flush()
-if Path.exists(dir + "/download/" + Filename):
+if Path.exists(dir / "download/" + Filename):
     print("Done!")
