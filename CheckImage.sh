aafc3d233b29faa16f5895ef9ec1dcf94802bfbd
bubbles-wow <917749218@qq.com>
2023-10-21 15:17:42 +0800

    add erofs support and simplify code

--- CheckImage.sh
+++ CheckImage.sh
@@ -2,86 +2,71 @@
 
 abort() {
     [ "$1" ] && echo -e "ERROR: $1"
-    echo "Build: an error has occurred, exit"
+    echo "CheckImage: an error has occurred, exit"
     exit 1
 }
 
-vhdx_to_raw_img() {
-    qemu-img convert -q -f vhdx -O raw "$1" "$2" || return 1
-    rm -f "$1" || return 1
-}
-
-ro_ext4_img_to_rw() {
-    resize_img "$1" "$(($(du --apparent-size -sB512 "$1" | cut -f1) * 2))"s || return 1
-    e2fsck -fp -E unshare_blocks "$1" || return 1
-    resize_img "$1" || return 1
-    return 0
-}
-
-resize_img() {
-    sudo e2fsck -pf "$1" || return 1
-    if [ "$2" ]; then
-        sudo resize2fs "$1" "$2" || return 1
-    else
-        sudo resize2fs -M "$1" || return 1
-    fi
-    return 0
-}
-
-check_image_type() {
-    local type
-    type=$(blkid -o value -s TYPE "$1")
-    echo "$type"
-}
-
 cd ./download
-file_name=$(ls *.Msixbundle)
-version=$(echo $file_name | cut -d'_' -f2)
-mv $file_name wsa.zip || abort
-unzip wsa.zip "WsaPackage_${version}_x64_Release-Nightly.msix" || abort
+FileName=$(ls *.Msixbundle) || abort
+PackageVersion=$(echo $FileName | cut -d'_' -f2) || abort
+mv $FileName wsa.zip || abort
+unzip wsa.zip "WsaPackage_${PackageVersion}_x64_Release-Nightly.msix" || abort
 rm -rf wsa.zip || abort
-mv "WsaPackage_${version}_x64_Release-Nightly.msix" wsa.zip || abort
-if [ "$version" \< "2302" ]; then
+mv "WsaPackage_${PackageVersion}_x64_Release-Nightly.msix" wsa.zip || abort
+if [ "PackageVersion" \< "2302" ]; then
     unzip wsa.zip system.img product.img || abort
 else
     unzip wsa.zip system.vhdx product.vhdx || abort
-    vhdx_to_raw_img system.vhdx system.img > /dev/null || abort
-    vhdx_to_raw_img product.vhdx product.img > /dev/null || abort
+    qemu-img convert -q -f vhdx -O raw system.vhdx system.img || abort
+    qemu-img convert -q -f vhdx -O raw product.vhdx product.img || abort
+    rm -rf system.vhdx product.vhdx || abort
 fi
 unzip wsa.zip Tools/kernel || abort
 mv ./Tools/kernel ./kernel || abort
 rm -rf wsa.zip ./Tools || abort
-image_type=$(check_image_type system.img) || abort
-if [ "$image_type" == "ext4" ]; then
-    ro_ext4_img_to_rw system.img > /dev/null || abort
-    ro_ext4_img_to_rw product.img > /dev/null || abort
+
+mkdir -p ./system || abort
+mkdir -p ./product || abort
+ImageType=$(blkid -o value -s TYPE system.img) || abort
+if [ "$ImageType" == "ext4" ]; then
+    sudo mount -t ext4 -o loop system.img ./system || abort
+    sudo mount -t ext4 -o loop product.img ./product || abort
+elif [ "$ImageType" == "erofs" ]; then
+    sudo mount -t erofs system.img ./system || abort
+    sudo mount -t erofs product.img ./product || abort
 else
     abort "system image is not supported"
 fi
-mkdir -p ./system || abort
-mkdir -p ./product || abort
-resize_img system.img > /dev/null || abort
-resize_img product.img > /dev/null || abort
-sudo mount -o loop system.img ./system || abort
-sudo mount -o loop product.img ./product || abort
 
-AndroidVersion=$(sed -n 's/^ro.build.version.release=//p' ./system/system/build.prop)
-API=$(sed -n 's/^ro.build.version.sdk=//p' ./system/system/build.prop)
-SeculityPatch=$(sed -n 's/^ro.build.version.security_patch=//p' ./system/system/build.prop)
-BuildID=$(sed -n 's/^ro.build.id=//p' ./system/system/build.prop)
-Kernel=$(file kernel | awk -F ': ' '{print $2}' | awk -F ' ' '{print $8, $10, $11, $12, $13, $14, $15, $16, $17, $18}' | cut -d',' -f1)
-WebViewVersion=$(aapt dump badging ./product/app/webview/webview.apk | grep -oP "versionName='\K[^']+")
+BuildProp=$(sudo cat ./system/system/build.prop)
+AndroidVersion=$(echo "$BuildProp" | sed -n 's/^ro.build.version.release=//p')
+API=$(echo "$BuildProp" | sed -n 's/^ro.build.version.sdk=//p')
+SeculityPatch=$(echo "$BuildProp" | sed -n 's/^ro.build.version.security_patch=//p')
+BuildID=$(echo "$BuildProp" | sed -n 's/^ro.build.id=//p')
 
-echo "AndroidVersion: $AndroidVersion"
-echo "WebViewVersion: $WebViewVersion"
-echo "API: $API"
-echo "SeculityPatch: $SeculityPatch"
-echo "BuildID: $BuildID"
-echo "Kernel: $Kernel"
+KernelInfo=$(file kernel | awk -F ', ' '{print $2}' | sed 's/version //; s/(\(.*\)) //')
+KernelMain=$(echo $KernelInfo | cut -d'-' -f1)
 
-AndroidInfoURL="https://developer.android.google.cn/about/versions/$AndroidVersion"
+WebViewVersion=$(sudo aapt dump badging ./product/app/webview/webview.apk | grep -oP "versionName='\K[^']+")
+
+if [ "$API" == "32" ]; then
+    AndroidVersion="12L"
+    AndroidInfoURL="https://developer.android.google.cn/about/versions/12/12L"
+else
+    AndroidInfoURL="https://developer.android.google.cn/about/versions/$AndroidVersion"
+fi
+
 SecPatchURL="https://source.android.com/security/bulletin/${SeculityPatch:0:9}1"
 SecPatchHead=$(curl -s -L $SecPatchURL | grep -oP '<title>\K[^<]+' | sed 's/&nbsp;//g')
+SecPatchDate=$(echo $SecPatchHead | grep -oP '\b[A-Z][a-z]+ \d{4}\b')
+
+echo "PackageVersion: $PackageVersion"
+echo "AndroidVersion: $AndroidVersion"
+echo "API: $API"
+echo "SeculityPatch: $SeculityPatch"
+echo "Kernel: $KernelInfo"
+echo "BuildID: $BuildID"
+echo "WebViewVersion: $WebViewVersion"
 
 Description="
 ## Details
@@ -90,11 +75,12 @@ Description="
     \`\`\`
     $SeculityPatch
     \`\`\`
-    Note: [$SecPatchHead]($SecPatchURL)
+    Note: [Android Security Bulletin—**$SecPatchDate** | Android Open Source Project]($SecPatchURL)
   - Kernel Version
     \`\`\`
-    $Kernel
+    $KernelInfo
     \`\`\`
+    Source: [latte-2/**$KernelMain**](https://github.com/microsoft/WSA-Linux-Kernel/)
   - Build ID: 
     \`\`\`
     $BuildID
